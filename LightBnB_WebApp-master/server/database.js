e11f709683e3294b5e18dd00f86ae54f4bdb2c21
Zak Warsame <zakwarsamee@gmail.com>
2021-09-16 20:35:23 +0000

    remove unuseful comments

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -17,16 +17,6 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 
-//  const queryString = `
-//  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-//  FROM students
-//  JOIN cohorts ON cohorts.id = cohort_id
-//  WHERE cohorts.name LIKE $1
-//  LIMIT $2;
-//  `;
-// const cohortName = process.argv[2];
-// const limit = process.argv[3] || 5;
-// const values = [`%${cohortName}%`, limit];
 
 const getUserWithEmail = function (email) {
   const queryString = `
